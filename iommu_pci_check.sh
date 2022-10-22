4315be122ca3895442f26b770b16921327a9966f
rafayet <78610949+awwmbCPmM9Q7xFfM@users.noreply.github.com>
2022-10-21 20:15:44 -0400

    Update iommu_pci_check.sh

--- iommu_pci_check.sh
+++ iommu_pci_check.sh
@@ -1,9 +1,12 @@
 #!/bin/zsh
 #From arch wiki: https://wiki.archlinux.org/title/PCI_passthrough_via_OVMF#Ensuring_that_the_groups_are_valid 
+rm output.txt
 shopt -s nullglob
 for g in $(find /sys/kernel/iommu_groups/* -maxdepth 0 -type d | sort -V); do
     echo "IOMMU Group ${g##*/}:" >> output.txt
     for d in $g/devices/*; do
         echo -e "\t$(lspci -nns ${d##*/})" >> output.txt
     done;
-done;
+done; 
+
+cat output.txt
